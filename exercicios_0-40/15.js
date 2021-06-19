c52e558521851412fb04ea07d5708b6135057f64
José H. Dall Asta <nekitbr3131@gmail.com>
2021-06-19 09:31:48 -0300

    Feito exercício.

---
+++ exercicios_0-40/15.js
@@ -0,0 +1,24 @@
+// 15)​Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console: “Não trabalhamos com este tipo de automóvel aqui”.
+
+
+
+function carros(produto){
+    switch (produto) {
+        case 'hatch':
+            return 'Compra efetuada com sucesso'
+
+        case 'sedans': case 'motocicletas': case 'caminhonetes':
+            return 'Tem certeza que não prefere este modelo?'
+
+        default:
+            return 'Não trabalhamos com este tipo de automóvel aqui.'
+    }
+}
+
+
+console.log(carros('hatch'))
+console.log(carros('sedans'))
+console.log(carros('motocicletas'))
+console.log(carros('caminhonetes'))
+console.log(carros('biz'))
+console.log(carros())
