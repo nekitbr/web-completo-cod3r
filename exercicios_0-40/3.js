11ef579d52ada8f1a9cf3b41123a2d1d3ec20668
José H. Dall Asta <nekitbr3131@gmail.com>
2021-06-19 05:34:32 -0300

    Feito exercicio 03.

---
+++ exercicios_0-40/3.js
@@ -0,0 +1,8 @@
+// 03)​ Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function topower(base=1, expoente=1){
+    return (base**expoente)
+}
+
+console.log(topower(4, 4))
+console.log(topower(2, 3))
