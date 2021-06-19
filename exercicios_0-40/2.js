a2153ecfc114cb61cb10127b28f883f327bf26a3
José H. Dall Asta <nekitbr3131@gmail.com>
2021-06-19 05:31:37 -0300

    Feito exercicio 02. Note que ele pede para abstrair as condições de existencia de um triângulo.

---
+++ exercicios_0-40/2.js
@@ -0,0 +1,22 @@
+/*
+02)​ Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+Equilátero: Os três lados são iguais.
+Isósceles:  Dois lados iguais.
+Escaleno: Todos os lados são diferentes.
+Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto ao tamanho de seus lados.
+(Neste exemplo deve-se abstrair as condições matemáticas de existência de um triângulo).
+*/
+
+let lado1 = 2
+let lado2 = 3
+let lado3 = 4
+
+if ((lado1 == lado2) && (lado1 == lado3)){
+    console.log('Equilátero!')
+}else if((lado1==lado2) || (lado1==lado3) || (lado2==lado3)){
+    console.log('Isósceles!')
+}else if(lado1 != lado2 && lado2 != lado3){
+    console.log('Escaleno!')
+}
+
+// Note que ele pede para abstrair as condições de existencia de um triângulo.
