27807cf7d9c1f6070084266286b17dc98386aa24
José H. Dall Asta <nekitbr3131@gmail.com>
2021-07-02 11:22:59 -0300

    Feito 05

---
+++ exercicios_0-40/Parte 2/05.js
@@ -0,0 +1,13 @@
+// https://prnt.sc/181kusc
+
+
+
+function maiorOuIgual(num1=0, num2=0){
+    return num1 >= num2
+}
+
+console.log(maiorOuIgual(0, 0))
+console.log(maiorOuIgual(0, "0"))
+console.log(maiorOuIgual(5, 1))
+console.log(maiorOuIgual(1, 5))
+console.log(maiorOuIgual(1, 1.1))
